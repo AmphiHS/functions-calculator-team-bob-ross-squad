f8628e62512984a1fc04dbeaccd2126774ba4809
[Hazazeal] <[jasielv275@gmail.com]>
2019-09-23 19:12:57 +0000

    adding more functions

--- partner-1.js
+++ partner-1.js
@@ -2,3 +2,18 @@
 const circArea = (radius) => {
     return (355/113) * (radius * radius);
 };
+const trapezoidArea = (height, base1, base2) => {
+    return  1/2 * height * (base1 + base2);
+};
+const arcLength = (radius, arc) => {
+    return  2*(355/113)*radius*(arc/360);
+};
+const sectorArea = (radius, angleOfArc) => {
+    return  (355/113)*(radius*radius)*(angleOfArc/360);
+};
+const sphereVolume = (radius) => {
+    return  (4/3)*(355/113)*(radius*radius*radius);
+};
+const coneVolume = (radius, height) => {
+    return  (1/3)*(355/113)*(radius*radius)*height;
+};
