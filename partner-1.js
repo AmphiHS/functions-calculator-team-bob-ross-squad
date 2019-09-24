5ed5e04232a2b9cd0f439f837d668bee6856c79e
[Hazazeal] <[jasielv275@gmail.com]>
2019-09-24 18:05:43 +0000

    even more functions

--- partner-1.js
+++ partner-1.js
@@ -17,3 +17,33 @@ const sphereVolume = (radius) => {
 const coneVolume = (radius, height) => {
     return  (1/3)*(355/113)*(radius*radius)*height;
 };
+const circumCircle = (radius) => {
+    return 2 * 355/115 * radius;
+};
+const areaTriangle = (base, height) => {
+    return 0.5 * base * height;
+};
+const areaRegPolygon = (numsides, apothem, sidelength) => {
+    return numsides * (0.5 * apothem * sidelength);
+};
+const volCylinder = (radius, height) => {
+    return 355/115 * radius * radius * height;
+};
+const pythagTheorem = (a,b) => {
+   return a * a + b * b;
+};
+const feetMeters = (feet) => {
+    return .305*feet;
+};
+const gallonsLiters = (gallons) => {
+    return 3.78*gallons;
+};
+const cToF = (Celcius) => {
+    return 1.8*Celcius+32;
+};
+const Slope = (x1,y1,x2,y2) => {
+    return (y2-y1)/(x2-x1);
+};
+const distance = (x1,y1,x2,y2) => {
+    return ((y2-y1)(y2-y1))+((x2-x1)(x2-x1));
+};
